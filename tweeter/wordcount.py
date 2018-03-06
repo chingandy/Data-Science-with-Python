99bb22e377c0636ffb143febe9156b070ef45dbb
Andy Wu <chingandywu@gmail.com>
2018-03-06 23:55:31 +0100

    update the recent progress.

---
+++ tweeter/wordcount.py
@@ -0,0 +1,37 @@
+import re
+
+def word_in_text(word, tweet):
+    word = word.lower()
+    text = tweet.lower()
+    match = re.search(word, tweet)
+
+    if match:
+        return True
+    return False
+
+# Initialize list to store tweet counts
+[clinton, trump, sanders, cruz] = [0, 0, 0, 0]
+
+# Iterate through df, counting the number of tweets in which
+# each candidate is mentioned
+for index, row in df.iterrows():
+    clinton += word_in_text('clinton', row['text'])
+    trump += word_in_text('trump', row['text'])
+    sanders += word_in_text('sanders', row['text'])
+    cruz += word_in_text('cruz', row['text'])
+# ---------------------------------------------------------
+# use the statistical data visualization library seaborn to plot a bar chart
+# Import packages
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Set seaborn style
+sns.set(color_codes=True)
+
+# Create a list of labels:cd
+cd = ['clinton', 'trump', 'sanders', 'cruz']
+
+# Plot histogram
+ax = sns.barplot(cd,[clinton, trump, sanders,cruz])
+ax.set(ylabel="count")
+plt.show()
